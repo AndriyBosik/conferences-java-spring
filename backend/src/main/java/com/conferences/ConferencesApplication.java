c27e77f1dd835f1c0002c06d118ce4cfe9d8f67c
AndriyBosik <andriybosik2002@ukr.net>
2021-09-09 22:50:57 +0300

    Initial commit

---
+++ backend/src/main/java/com/conferences/ConferencesApplication.java
@@ -0,0 +1,13 @@
+package com.conferences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferencesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferencesApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/conferences/ConferencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conferences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferencesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
